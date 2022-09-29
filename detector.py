ef665533f54f32238d21f07a4fe872d6d6f26c65
Clara <zl@dynaikon.com>
2022-09-29 10:35:52 +0100

    WIP: refine fft

--- detector.py
+++ detector.py
@@ -6,7 +6,7 @@ import numpy as np
 from scipy import fft
 
 
-alarm_freq = 3500
+alarm_freq = 3000
 bandwidth = 50
 volume_gate = .5
 alert_window = 8
@@ -16,16 +16,22 @@ sr = 44100
 n_samples = 4096
 
 
-def _find_loudest_freq(signal, sr, normalise=True):
+def _fft(signal, sr, normalise=True):
     # scale to between 0 and 1 if signal is 16 bit numbers
     norm_sig = (signal/32768.0) if normalise else signal
     yf = fft.rfft(norm_sig)
     xf = fft.rfftfreq(len(norm_sig), 1/sr)
-    return dict(loudest=xf[np.argmax(np.abs(yf))], volume=np.abs(yf).max())
+    result = dict(
+        loudest_freq = xf[np.argmax(np.abs(yf))],
+        loudest_intensity = np.abs(yf).max(),
+        intensity = yf,
+        freq = xf
+    )
+    return result
 
 
 def _generate_sine(freq, sr, dur):
-    """generate 16 bit sine wave for testing
+    """generate sine wave for testing
 
     :param freq: frequency in Hz of sine wave
     :type freq: int or float
